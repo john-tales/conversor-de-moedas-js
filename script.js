2c12ed7a306566b4f3c110b4ccfd0b720ebe76df
john-tales <john.tales@hotmail.com>
2024-07-09 11:59:12 -0300

    change img-innerhtml from titles of coins

--- script.js
+++ script.js
@@ -3,18 +3,18 @@
 
 
 const convertButton = document.querySelector(".convert-button")
+const currencySelected = document.querySelector(".currency-select")
 
+const dolartoday = 5.2
+const eurotoday = 6.2
+const libratody = 7.2
+const bitcointoday = 352096.00
+const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+const currencyValueConvertd = document.querySelector(".currency-value")
 
 function convertValues(){
+    
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencySelected = document.querySelector(".currency-select")
-    const dolartoday = 5.2
-    const eurotoday = 6.2
-    const libratody = 7.2
-    const bitcointoday = 352096.00
-
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueConvertd = document.querySelector(".currency-value")
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
@@ -37,10 +37,7 @@ function convertValues(){
                 currency: "eur"
             }).format(inputCurrencyValue / eurotoday)     
         }
-        //new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(
-          //  number,
           
-        
         if(currencySelected.value == "libra"){
             currencyValueConvertd.innerHTML = new Intl.NumberFormat("en-GB",{
                 style: "currency",
@@ -54,9 +51,48 @@ function convertValues(){
                 currency: "btc"
             }).format(inputCurrencyValue / bitcointoday)     
         }
-
-
         
+}
 
-} 
+
+function changecurrency(){
+    const currencyName = document.getElementById("currency-name")
+    const imgcurrency = document.querySelector(".imgcurrency")
+
+    if (currencySelected.value == "dolar") {
+
+        currencyName.innerHTML="Dolar americano"
+        imgcurrency.src="./assets/Bandera-circular-de-Estados-Unidos.png"
+        
+    }
+
+    if (currencySelected.value == "euro") {
+
+        currencyName.innerHTML="Euro"
+        imgcurrency.src="./assets/euro.png"
+        
+    }
+
+    if (currencySelected.value == "libra") {
+
+        currencyName.innerHTML="libra"
+        imgcurrency.src="./assets/libra 1.png"
+        
+    }
+
+    if (currencySelected.value == "bitcoin") {
+
+        currencyName.innerHTML="Bitcoin"
+        imgcurrency.src="./assets/bitcoin 1.png"
+        
+    }
+}
+
+
+
+
+
+
+currencySelected.addEventListener("change", changecurrency)
 convertButton.addEventListener("click", convertValues) 
+
